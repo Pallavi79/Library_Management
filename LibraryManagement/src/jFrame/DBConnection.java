ec8f85a932715f5544ef4d90af30ce3bea57d4f7
Pallavi <pallavi3749@gmail.com>
2023-01-10 23:15:06 +0530

    signup completed

---
+++ LibraryManagement/src/jFrame/DBConnection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jFrame;
+
+/**
+ *
+ * @author HP
+ */
+
+import java.sql.*;
+
+public class DBConnection {
+    static Connection con = null;
+    
+    public static Connection getConnection(){
+        try{
+            
+            Class.forName( "com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection( "jdbc:mysql://localhost:3307/library_ms", "root", "");
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+    
+}
